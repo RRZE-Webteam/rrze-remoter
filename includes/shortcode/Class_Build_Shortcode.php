14ac17db24cb09248d1ecc4b55a48f9914ccffd4
bmehler <bernhard.mehler@gmail.com>
2017-10-26 17:24:58 +0200

    - Table ohne Pagination mit dynamischen Spalten - Neuer Shortcode Paramter show

--- includes/shortcode/Class_Build_Shortcode.php
+++ includes/shortcode/Class_Build_Shortcode.php
@@ -38,7 +38,8 @@ class Class_Build_Shortcode {
             'folder'    => '0',
             'date'      => '0',
             'type'      => '0',
-            'filesize'  => '0'
+            'filesize'  => '0',
+            'show'      => 'folder,download,type,size,date,name'
         ), $atts );
         
         return $this->query_args($this->remote_server_shortcode);
@@ -82,12 +83,13 @@ class Class_Build_Shortcode {
                 $view = $this->remote_server_shortcode['view'];
                 $recursiv = $this->remote_server_shortcode['recursiv'];
                 $filetype = $this->remote_server_shortcode['filetype'];
-                $download = $this->remote_server_shortcode['download'];
+                /*$download = $this->remote_server_shortcode['download'];
                 $folder_column = $this->remote_server_shortcode['folder'];
                 $date_column = $this->remote_server_shortcode['date'];
                 $type_column = $this->remote_server_shortcode['type'];
-                $size_column = $this->remote_server_shortcode['filesize'];
-                $link = $this->remote_server_shortcode['link'];
+                $size_column = $this->remote_server_shortcode['filesize'];*/
+                $show_columns = $this->remote_server_shortcode['show'];
+                //$link = $this->remote_server_shortcode['link'];
                 $this->remote_data = Class_Grab_Remote_Files::get_files_from_remote_server($this->remote_server_shortcode, $domain, $api_key);
                 
                 /*echo '<pre>';
--- includes/templates/table_without_pagination.php
+++ includes/templates/table_without_pagination.php
@@ -1,145 +1,121 @@
 <?php
 echo '<h3>Normale Tabellenansicht</h3>';
 
-$list = '<ul>';
+$data = $this->remote_data;
 
-$id = uniqid();
-
-$output = '<div><table><tr>';
-$output .= $folder_column == 1 ? '<th>Verzeichnis</th>' : '';
-$output .= '<th>Name</th>';
-$output .= $date_column == 1 ? '<th>Änderungsdatum</th>' : '';
-$output .= $type_column == 1 ? '<th>Dateityp</th>' : '';
-$output .= $size_column == 1 ? '<th>Dateigröße</th>' : '';
-$output .= $download == 1 ? '<th>Download</th>' : '';
-$output .= '</tr>';
-
-/*$i = 0;
-
-
-
-
-for($j = 0; $j < sizeof($this->remote_data); $j++) {
-    $from_a = $this->remote_data[$j]['dir'];
-    $titel_a = explode("/",$from_a);
-    $folder_a[] = $titel_a[count($titel_a)-1];
+if (!function_exists('getHeaderData')) {
+    function getHeaderData($columns) {
+        $columns = explode(",", $columns);
+        return $columns;
+    }
 }
 
-echo '<pre>';
-//print_r($folder_a);
-echo '</pre>';
+$headerColumns = getHeaderData($show_columns);
 
-$unique = array_unique($folder_a);
-
-//sort($unique);
-
-echo '<pre>';
-//print_r($unique);
-echo '</pre>';
-
-$out = array();
-foreach ($this->remote_data as $key => $value){
-    $out[] = array_merge((array)$folder_a[$key], $value);
-}
-
-for($z = 0; $z < sizeof($out); $z++) {
-    $out[$z]['folder'] = $out[$z][0];
-    unset($out[$z][0]);
-}
-
-
-echo '<pre>';
-//print_r($out);
-echo '</pre>';*/
-
-//rsort($this->remote_data);
-
-//natcasesort($this->remote_data);
-
-/*usort($this->remote_data, function($x, $y) {
-     return strcasecmp($x['name'] , $y['name']);
-});*/
-
-sort($this->remote_data);
-
-foreach ($this->remote_data as $key => $value) {
-    
-    $bytes = $value['size'];
+if (!function_exists('createHeader')) {
+    function createHeader($columns, $data) {
+        
+        $t  = '<div>';
+        $t .= '<table>';
+        $t .= '<tr>';
+        
+        foreach($columns as $key => $column) {
+        
+            switch($column) {
+                case 'size':
+                    $t .= '<th>'. ucfirst($column) .'</th>';
+                    break;
+                case 'type':
+                    $t .= '<th>'. ucfirst($column) .'</th>';
+                    break;
+                case 'download':
+                    $t .= '<th>'. ucfirst($column) .'</th>';
+                    break;
+                case 'folder':
+                    $t .= '<th>'. ucfirst($column) .'</th>';
+                    break;
+                case 'name':
+                    $t .= '<th>'. ucfirst($column) .'</th>';
+                    echo '';
+                case 'date':
+                    $t .= '<th>'. ucfirst($column) .'</th>';
+                    echo '';    
+            }
+        }
+        
+        $t .= '</tr>';
+        
+        for($i = 0; $i < sizeof($data); $i++) {
             
-    if ($bytes>= 1073741824) {
-        $size = number_format($bytes / 1073741824, 2) . ' GB';
-    } elseif ($bytes >= 1048576) {
-       $size = number_format($bytes / 1048576, 2) . ' MB';
-    } elseif ($bytes >= 1024) {
-        $size = number_format($bytes / 1024, 0) . ' KB';
-    } elseif ($bytes > 1) {
-        $size = $bytes . ' bytes';
-    } elseif ($bytes == 1) {
-        $size = '1 byte';
-    } else {
-        $size = '0 bytes';
+            $t .= '</tr>';
+        
+            foreach($columns as $key => $column) {
+
+               switch($column) {
+                    case 'size':
+                        $t .= '<td>' . formatSize($data[$i]['size']) . '</td>';
+                        break;
+                    case 'type':
+                        $t .= '<td>' . $data[$i]['extension'] .'</td>';
+                        break;
+                    case 'download':
+                        $t .= '<td><a href=""  download><i class="fa fa-arrow-circle-down" aria-hidden="true"></i></a></td>';
+                        break;
+                    case 'folder':
+                        $t .= '<td>' . getFolder($data[$i]['dir']) . '</td>';
+                        break;
+                    case 'name':
+                        $t .= '<td>' . basename($data[$i]['path']) .'</td>';
+                        echo ''; 
+                    case 'date':
+                        $t .= '<td>' . $data[$i]['access_time'] .'</td>';
+                        echo ''; 
+                }
+
+            }
+            
+            $t .= '</tr>';
+            
+        }
+       
+        $t .= '</table></div>';
+        echo $t;
+        
     }
-    
-    $file = 'http://'. $url['host'] . $value['image'] . '';
-    $test = getimagesize('http://remoter.dev/images/butterfly_abstract_colorful_patterns_97225_2560x1440.jpg', $info);
-    $iptc = iptcparse($info["APP13"]);
-    if (is_array($iptc)) {
-        $data['headline']             = $iptc["2#105"][0];
-        $data['documentTitle']        = $iptc["2#120"][0];
-        $data['graphic_name']         = $iptc["2#005"][0];
-        $data['urgency']              = $iptc["2#010"][0];
-        $data['category']             = $iptc["2#015"][0];
-        $data['supp_categories']      = $iptc["2#020"][0];
-        $data['spec_instr']           = $iptc["2#040"][0];
-        $data['creation_date']        = $iptc["2#055"][0];
-        $data['authorByline']         = $iptc["2#080"][0];
-        $data['authorTitle']          = $iptc["2#085"][0];
-        $data['city']                 = $iptc["2#090"][0];
-        $data['state']                = $iptc["2#095"][0];
-        $data['country']              = $iptc["2#101"][0];
-        $data['otr']                  = $iptc["2#103"][0];
-        $data['source']               = $iptc["2#110"][0];
-        $data['photo_source']         = $iptc["2#115"][0];
-    }
-    
-    /*echo '<pre>';
-    //print_r($data);
-    echo '</pre>';*/
-    
-   /* $from = $this->remote_data[$i]['dir'];
-    $titel = explode("/",$from);
-    $folder = $titel[count($titel)-1];
-    $i++;
-    */
-    
-   
-    
-    $imageicon = $value['extension'] == 'pdf' ? '<i class="fa fa-file-pdf-o" aria-hidden="true"></i>' : '<i class="fa fa-file-image-o" aria-hidden="true"></i>' ;
-    
-    $filename = $value['basename'];
-    
-    $output .= '<tr>';
-    
-    if ($folder_column)  $output .= '<td>' . $folder . '</td>';
-    
-    if ($link) {
-        $output .= '<td><a class="lightbox" rel="lightbox-' . $id . '" href="http://'. $url['host'] . $value['image'] . '">' . substr($value['basename'], 0, strrpos($value['basename'], '.')) . '</a></td>';
-    } else {
-        $output .= '<td>' . ((substr($value['basename'],0,1) === 'G') ? substr($value['basename'],13) : $value['basename'] ). '</td>';
-    }
-    
-    if ($date_column) { 
-        $date = new DateTime();
-        $date->setTimestamp($value['change_time']);
-        $output .= '<td>' . $date->format('j. F Y') . '</td>';
-    }
-    
-    if ($type_column) $output .= '<td>'. $imageicon . '</td>';
-    if ($size_column) $output .= '<td>' . $size .  '</td>';
-    if ($download) $output  .= '<td><a href="http://'. $url['host'] . $value['image'] . '" title="Rechtsklick für Download" download><i class="fa fa-arrow-circle-down" aria-hidden="true"></i></a></td>';
- 
 }
 
-$output .= '</table></div>';
+if (!function_exists('formatSize')) {
+    function formatSize($bytes) {
 
-echo $output;
+        if ($bytes>= 1073741824) {
+            $size = number_format($bytes / 1073741824, 2) . ' GB';
+        } elseif ($bytes >= 1048576) {
+           $size = number_format($bytes / 1048576, 2) . ' MB';
+        } elseif ($bytes >= 1024) {
+            $size = number_format($bytes / 1024, 0) . ' KB';
+        } elseif ($bytes > 1) {
+            $size = $bytes . ' bytes';
+        } elseif ($bytes == 1) {
+            $size = '1 byte';
+        } else {
+            $size = '0 bytes';
+        }
+        
+        return $size;
+    }
+}
+
+
+if (!function_exists('getFolder')) {
+    function getFolder($directory) {
+ 
+        $titel = explode("/", $directory);
+        $folder = $titel[count($titel)-1];
+        
+        return $folder;
+    
+    }
+}
+      
+$headerColumns = getHeaderData($show_columns);
+$header = createHeader($headerColumns, $data);
