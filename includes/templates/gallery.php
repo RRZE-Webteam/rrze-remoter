a03e067229e896385d0cb7349a92287665d5fb0e
bmehler <bernhard.mehler@gmail.com>
2017-11-03 11:34:11 +0100

    - Gallery mit IPTC Daten möglich.

--- includes/templates/gallery.php
+++ includes/templates/gallery.php
@@ -1,40 +1,88 @@
 <?php
 
-/*echo '<pre>';
-print_r($this->remote_data);
-echo '</pre>';*/
-
-
-echo '<h3>Galerie</h3>';
-    
 $id = uniqid();
+$data = $this->remote_data;
 
-echo '<div id="slider-' . $id . '" class="image-gallery-slider">
-<ul class="slides">';
-foreach ($this->remote_data as $key => $value) {
-echo '<li><img src="http://'. $url['host'] . $value['image'] . '"/>
-<div class="gallery-image-caption">Bild in Originalgröße (1153px). Ausrichtung keine.<br /><span class="linkorigin">(<a href="http://'. $url['host'] . $value['image'] . '"  title="Bild in Originalgröße (1153px). Ausrichtung keine." class="lightbox" rel="lightbox-601862376">Vergrößern</a>)</span></div>
-</li>';
-}
+function createSlider($data, $url, $id) {
 
-echo '
-</ul>
-</div>';
+    $g = '<h3>Galerie</h3>';
+    $g .= '<div id="slider-' . $id . '" class="image-gallery-slider">';
+    $g .= '<ul class="slides">';
+    
+    foreach ($data as $key => $value) {
         
-echo '<div id="carousel-' . $id . '" class="image-gallery-carousel">
-<ul class="slides">';
-foreach ($this->remote_data as $key => $value) {
-echo '<li><img src="http://'. $url['host'] . $value['image'] . '" width="120" height="80" alt=""/></li>';
+        $path       = 'http://'. $url['host'] . $value['image'] . '';
+        $imginfo    = getimagesize($path, $info);
+        $iptcdata    = iptcparse($info["APP13"]);
+        
+        $g .= '<li><img src="http://'. $url['host'] . $value['image'] . '"/>';
+        $g .= '<div class="gallery-image-caption">Bild in Originalgröße (1153px). Ausrichtung keine.<br />';
+        $g .= '<span class="linkorigin">';
+        $g .= '(<a href="http://'. $url['host'] . $value['image'] . '"  title="'. $iptcdata["2#105"][0] . '<br/>' . $iptcdata["2#120"][0] . '<br/>' . $iptcdata["2#085"][0] .'" class="lightbox" rel="lightbox-601862376">Vergrößern</a>)</span></div>';
+        $g .= '<div>' . $iptcdata["2#105"][0] . '<br/>' . $iptcdata["2#120"][0] . '<br/>' . $iptcdata["2#085"][0] . '</div>';
+        $g .= '</li>';
+    
+    }
+
+    $g .= '</ul></div>';
+    echo $g;
+    
 }
-echo '</ul></div>';
+
+
+
+function createCarousel($data, $url, $id) {
+        
+    $c = '<div id="carousel-' . $id . '" class="image-gallery-carousel">';
+    $c .= '<ul class="slides">';
+    
+    foreach ($data as $key => $value) {
+        $c .= '<li><img src="http://'. $url['host'] . $value['image'] . '" width="120" height="80" alt=""/></li>';
+    }
+        
+    $c .='</ul></div>';
+    echo $c;
+}
+
+/*function getIptcData($data, $url) {
+    
+    $path       = 'http://'. $url['host'] . $value['image'] . '';
+    $imginfo    = getimagesize($path, $info);
+    $iptcdata    = iptcparse($info["APP13"]);
+    
+    if (is_array($iptcdata)) {
+        $data['headline']             = $iptc["2#105"][0];
+        $data['documentTitle']        = $iptc["2#120"][0];
+        $data['graphic_name']         = $iptc["2#005"][0];
+        $data['urgency']              = $iptc["2#010"][0];
+        $data['category']             = $iptc["2#015"][0];
+        $data['supp_categories']      = $iptc["2#020"][0];
+        $data['spec_instr']           = $iptc["2#040"][0];
+        $data['creation_date']        = $iptc["2#055"][0];
+        $data['authorByline']         = $iptc["2#080"][0];
+        $data['authorTitle']          = $iptc["2#085"][0];
+        $data['city']                 = $iptc["2#090"][0];
+        $data['state']                = $iptc["2#095"][0];
+        $data['country']              = $iptc["2#101"][0];
+        $data['otr']                  = $iptc["2#103"][0];
+        $data['source']               = $iptc["2#110"][0];
+        $data['photo_source']         = $iptc["2#115"][0];
+        
+    }
+    
+    return $data;
+
+}*/
+
+createSlider($data, $url, $id);
+createCarousel($data, $url, $id);
+
 ?>
-        
 
-<?php ?>
 <script type="text/javascript">
 jQuery(document).ready(function($) {
   $("#carousel-<?php echo $id ?>").flexslider({
-    maxItems: 5,
+    maxItems: 7,
     selector: "ul > li",
     animation: "slide",
     keyboard: true,
