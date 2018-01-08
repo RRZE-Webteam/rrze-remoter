90460b673530a724e016e51dc26bba25e7b20266
bmehler <bernhard.mehler@gmail.com>
2018-01-08 17:02:27 +0100

    - erste Anpassungen für RRZE Theme - rrze-styles.css hinzugefügt - wird mit wp_get_theme() in rrze-remoter.php bestimmt.

--- rrze-remoter.php
+++ rrze-remoter.php
@@ -218,12 +218,28 @@ function custom_libraries_scripts() {
     wp_register_script( 'rrze-remoter-mainjs', plugins_url( 'rrze-remoter/assets/js/rrze-remoter-main.js', dirname(__FILE__)), array('jquery'),'', true);
     wp_register_script( 'rrze-remoter-scriptsjs', plugins_url( 'rrze-remoter/assets/js/rrze-remoter-scripts.js', dirname(__FILE__)), array('jquery'),'', true);
     wp_register_style( 'rrze-remoter-stylescss', plugins_url( 'rrze-remoter/assets/css/styles.css', dirname(__FILE__) ) );
+    wp_register_style( 'rrze-remoter-rrze-theme-stylescss', plugins_url( 'rrze-remoter/assets/css/rrze-styles.css', dirname(__FILE__) ) );
+    wp_register_script( 'flexsliderjs', plugins_url( 'rrze-remoter/assets/js/jquery.flexslider.js', dirname(__FILE__)), array('jquery'),'', true);
+    wp_register_script( 'fancyboxjs', plugins_url( 'rrze-remoter/assets/js/jquery.fancybox.js', dirname(__FILE__)), array('jquery'),'', true);
     
     if( is_page() && has_shortcode( $post->post_content, 'remoter') ) {
         wp_enqueue_script( 'rrze-remoter-mainjs' );
         wp_enqueue_script( 'rrze-remoter-scriptsjs' );
-        wp_enqueue_style( 'rrze-remoter-stylescss' );
+        
+        $current_theme = wp_get_theme();
+        if($current_theme == 'RRZE 2015') {
+            wp_enqueue_script( 'flexsliderjs' );
+            wp_enqueue_script( 'fancyboxjs' );
+            wp_enqueue_style( 'rrze-remoter-rrze-theme-stylescss' );
+        } else {
+            wp_enqueue_style( 'rrze-remoter-stylescss' );
+        } 
+       
     }
+    
+    echo '<pre>';
+    echo $current_theme;
+    echo '</pre>';
     
     wp_localize_script( 'rrze-remoter-mainjs', 'frontendajax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));
 }
