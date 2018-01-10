5d3e19e6488187405d045c116e9c37aa792d94c9
bmehler <bernhard.mehler@gmail.com>
2018-01-10 16:07:36 +0100

    - SASS-Dateien neu organisiert und umgewandelt

--- includes/templates/imagetable.php
+++ includes/templates/imagetable.php
@@ -9,7 +9,7 @@ $output .= '<ul class="grid">';
 
 
 foreach ($this->remote_data as $key => $value) {
-    $output .= '<li style="height: 183px;"><a href="http://'.  $domain . $value['dir'] . $value['name'] . '" class="lightbox" rel="lightbox-' . $id .'" ><img src="http://'. $domain . $value['dir'] . $value['name'] . '" width="120" height="47" alt=""></a></li>';
+    $output .= '<li style="height: 120px;"><a href="http://'.  $domain . $value['dir'] . $value['name'] . '" class="lightbox" rel="lightbox-' . $id .'" ><img src="http://'. $domain . $value['dir'] . $value['name'] . '" width="120" height="47" alt=""></a></li>';
 }
 
 
