e96d247433d31c2d879a49a70bcae8387d05f20d
bmehler <bernhard.mehler@gmail.com>
2018-05-17 11:56:58 +0200

    - List-View aktualisiert - Umlaute aktualisiert

--- includes/templates/list.php
+++ includes/templates/list.php
@@ -4,54 +4,30 @@ $list = '<ul>';
 
 $id = uniqid();
 
-$sortOrderby = ($orderby === 'size') ? 'size' : (($orderby === 'date') ? 'date' : 'name');
+$sortOrderby = ($orderby === 'size' ? 'size' : 'name');
 $sortOrder = ($order === 'asc' ? SORT_ASC : SORT_DESC);
 array_multisort(array_column($data, $sortOrderby), $sortOrder , $data);
 
 foreach ($data as $key => $value) {
-
-    $list .= '<li>' . getIcon($value['name']) . ' <a href="http://'. $domain . $value['dir'] . $value['name'] . '">' . replaceCharacter(changeUmlauts($value['name'])) . '</a> (' . getSize($value['size']) . ')</li>';
+    
+    if($value['extension'] == 'pdf') { 
+        $icon ='<i class="fa fa-file-pdf-o" aria-hidden="true"></i>';
+    } elseif ($value['extension'] == 'pptx' || $value['extension'] =='ppt') { 
+        $icon ='<i class="fa fa-file-powerpoint-o" aria-hidden="true"></i>';
+    } elseif ($value['extension'] == 'docx' || $value['extension'] =='doc' ) { 
+        $icon ='<i class="fa fa-file-word-o" aria-hidden="true"></i>';
+    } elseif ($value['extension'] == 'xlsx' || $value['extension'] =='xls') { 
+        $icon ='<i class="fa fa-file-excel-o" aria-hidden="true"></i>';
+    } elseif ($value['extension'] == 'mpg' || $value['extension'] =='mpeg'|| $value['extension'] =='mp4' || $value['extension'] =='m4v') { 
+        $icon ='<i class="fa fa-file-movie-o" aria-hidden="true"></i>';
+    } else { 
+        $icon ='<i class="fa fa-file-image-o" aria-hidden="true"></i>';
+    }
+    
+    $list.= '<li>' . $icon . ' <a href="http://'. $domain . $value['dir'] . $value['name'] . '">' . RRZE\Remoter\Class_Help_Methods::replaceCharacterList(RRZE\Remoter\Class_Help_Methods::changeUmlautsList($value['name'])) . '</a> (' . RRZE\Remoter\Class_Help_Methods::formatSize($value['size']) . ')</li>';
 
 }
 
 $list .= '</ul>';
 
 echo $list;
-
-function getIcon($filename) {
-    //$extension = $filename;
-    $extension = substr( strrchr($filename, '.'), 1);
-    if($extension == 'pdf') { 
-        return '<i class="fa fa-file-pdf-o" aria-hidden="true"></i>';
-    } elseif ($extension == 'pptx' || $extension =='ppt') { 
-        return '<i class="fa fa-file-powerpoint-o" aria-hidden="true"></i>';
-    } elseif ($extension == 'docx' || $extension =='doc' ) { 
-        return '<i class="fa fa-file-word-o" aria-hidden="true"></i>';
-    } elseif ($extension == 'xlsx' || $extension =='xls') { 
-        return '<i class="fa fa-file-excel-o" aria-hidden="true"></i>';
-    } elseif ($extension == 'mpg' || $extension =='mpeg'|| $extension =='mp4' || $extension =='m4v') { 
-        return '<i class="fa fa-file-movie-o" aria-hidden="true"></i>';
-    } else { 
-        return '<i class="fa fa-file-image-o" aria-hidden="true"></i>';
-    }
-}
-
-function getSize($size) {
-    return RRZE\Remoter\Class_Help_Methods::formatSize($size);
-}
-
-function replaceCharacter($name) {
-    $newName = str_replace('_',' ', $name);
-    return $newName;
-}
-
-function changeUmlauts($filename) {
-    
-    $str = str_replace(
-        array('ae','oe','ue','Ae','Oe','Ue','Ã', 'ss'), 
-        array( 'ä','ö','ü','Ä','Ö','Ü','Ä', 'ß'),
-        $filename
-    );  
-
-    return $str;
-}
